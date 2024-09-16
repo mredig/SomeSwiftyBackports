2e9c2dcd1ab40e322390eaf43a206193bd4d5d16
Michael Redig <mredig@gmail.com>
2024-09-16 17:03:50 -0500

    (refactor) test additional sleep durations

--- Tests/SSBTaskSleepTests/SSBTaskSleepTests.swift
+++ Tests/SSBTaskSleepTests/SSBTaskSleepTests.swift
@@ -15,6 +15,49 @@ func testTaskSecondSleep() async throws {
 	#expect((0.93..<1.07).contains(difference))
 }
 
+@Test
+func testTaskMSSleep() async throws {
+	let start = getTime()
+
+	try await Task.sleep(backportDuration: .milliseconds(500))
+
+	let end = getTime()
+
+	let difference = abs(end.seconds - start.seconds)
+	print(difference)
+
+	#expect((0.49..<0.55).contains(difference))
+}
+
+@Test
+func testTaskuSSleep() async throws {
+	let start = getTime()
+
+	try await Task.sleep(backportDuration: .microseconds(5000))
+
+	let end = getTime()
+
+	let difference = abs(end.seconds - start.seconds)
+	print(difference)
+
+	#expect((0.005..<0.01).contains(difference))
+}
+
+@Test
+func testTaskNSSleep() async throws {
+	let start = getTime()
+
+	try await Task.sleep(backportDuration: .nanoseconds(5_000_000))
+
+	let end = getTime()
+
+	let difference = abs(end.seconds - start.seconds)
+	print(difference)
+
+	#expect((0.005..<0.01).contains(difference))
+}
+
+
 private func getTime() -> timespec {
 	var timeHolder = timespec()
 	clock_gettime(CLOCK_MONOTONIC_RAW, &timeHolder)
